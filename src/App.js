d56b2377e7be38ca58fbeb2da0788da69c1a0c98
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2019-02-06 16:01:35 -0500

    fixed data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 
+import pizzaData from './pizza.json';
 import Filter from './component/Filter';
 
-const getPizzaUrl = './pizza.json';
+// const getPizzaUrl = './pizza.json';
 
 class App extends React.Component {
   constructor() {
@@ -16,19 +18,20 @@ class App extends React.Component {
     this.handleSortPizzas = this.handleSortPizzas.bind(this);
   }
 
+  // fetchPizzaList() {
+  //   var self = this;
+  //   self.setState({ loading: true });
+  //   axios.get(pizzaData).then(function(data) {
+  //     self.setState({
+  //       pizzas: data.pizzas,
+  //       loading: false
+  //     });
+  //   });
+  // }
+
   fetchPizzaList() {
-    var self = this;
-    self.setState({ loading: true });
-    fetch(getPizzaUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        self.setState({
-          pizzas: data.pizzas,
-          loading: false
-        });
-      });
+    console.log(pizzaData);
+    this.setState({ pizzas: pizzaData.pizzas });
   }
 
   handleSortPizzas() {
